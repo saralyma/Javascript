5f432d88b8ab82f376a5f4680ed8e3e0a546e673
saralyma <84419912+saralyma@users.noreply.github.com>
2021-06-30 14:06:23 -0300

    Add files via upload

---
+++ proj008.js
@@ -0,0 +1,10 @@
+var vel = 10.8
+console.log(`a velocidade do seu carro e  ${vel}  km /h`)
+if (vel >60 ) { 
+    console.log( 'voce ultrapassou a velocidade permitida . MULTADO!' )
+
+}
+console.log ('Dirija sempre usando cinto de segurança!')
+
+// condicao simples
+
---
+++ projx009.js
@@ -0,0 +1,9 @@
+var pais = 'Bolivia'
+console.log(`vivendo no pais ${pais} `)
+if ( pais != 'Brasil') { 
+    console.log ('Voce e Estrangeiro')
+
+}  else {
+    console.log ('voce e Brasileiro')
+   
+}
