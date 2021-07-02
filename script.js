de4e7a31346da267b563eb82b0d346ce8beab835
saralyma <84419912+saralyma@users.noreply.github.com>
2021-07-02 16:46:43 -0300

    Add files via upload
    um lindo descanso de tela , com hora do dia , e saudação de Bom dia , boa tarde e boa noite !

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar() {
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora =  20 //data.getHours()
+msg.innerHTML = `agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    img.scr = 'manha.png'
+    document.body.style.background = '#e2cd9f'
+} 
+else if (hora >= 12 && hora <= 18) {
+    img.src = 'tarde.png'
+    document.body.style.background = '#b9846f'
+}
+else {
+    img.src ='noite.png'
+    document.body.style.background = '#515154'
+}
+}
